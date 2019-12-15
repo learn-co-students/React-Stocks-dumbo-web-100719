90b4ded074fe69ae10751519ff22caec2f8b866d
RubenV-Dev <r.will16@hotmail.com>
2019-12-15 10:16:18 -0500

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,12 +3,16 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+  renderPortfolio = () => {
+    return this.props.portfolio.map((stock, index) => <Stock key={`${stock.name}-${index}`} stock={stock} handleClick={this.props.removeStock}/>)
+    }
+
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            this.renderPortfolio()
           }
       </div>
     );
