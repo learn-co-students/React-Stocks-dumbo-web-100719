53d223028ceffb75487bdc6d96a7742e51b85c30
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-12-17 01:10:57 -0500

    Done

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,42 @@
-import React from 'react';
+import React from "react";
 
-const SearchBar = () => {
-  return (
-    <div>
+const SearchBar = props => {
+	return (
+		<div>
+			<strong>Sort by:</strong>
+			<label>
+				<input
+					type="radio"
+					value="Alphabetically"
+					checked={null}
+					name="radio"
+					onChange={e => props.sortStocks(e.target.value)}
+				/>
+				Alphabetically
+			</label>
+			<label>
+				<input
+					type="radio"
+					value="Price"
+					checked={null}
+					name="radio"
+					onChange={e => props.sortStocks(e.target.value)}
+				/>
+				Price
+			</label>
+			<br />
 
-      <strong>Sort by:</strong>
-      <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
-        Alphabetically
-      </label>
-      <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
-        Price
-      </label>
-      <br/>
-
-      <label>
-        <strong>Filter:</strong>
-        <select onChange={null}>
-          <option value="Tech">Tech</option>
-          <option value="Sportswear">Sportswear</option>
-          <option value="Finance">Finance</option>
-        </select>
-      </label>
-
-
-    </div>
-  );
-}
-
+			<label>
+				<strong>Filter:</strong>
+				<select onChange={e => props.filterStocks(e.target.value)}>
+					<option value="All">All</option>
+					<option value="Tech">Tech</option>
+					<option value="Sportswear">Sportswear</option>
+					<option value="Finance">Finance</option>
+				</select>
+			</label>
+		</div>
+	);
+};
 
 export default SearchBar;
