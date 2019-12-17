c24772edfcce2bacd14fde50b7b86075785415bb
Vannida Lim <vannida.lim@gmail.com>
2019-12-16 21:33:31 -0500

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -8,7 +8,10 @@ class PortfolioContainer extends Component {
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            this.props.portfolio.map((stock, idx) => 
+            <Stock key={`${stock.name}-${idx}`} 
+            stock={stock} 
+            handleClick={this.props.deleteStock}/>)
           }
       </div>
     );
